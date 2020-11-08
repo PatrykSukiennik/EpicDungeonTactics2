31a3eeccf589cdf56c9c02aa26d714a80dbffaea
Patryk Sukiennik <patryksukiennik27@gmail.com>
2020-11-08 09:55:04 +0100

    Map generating preparations vol. 2

---
+++ world_creating_elements/world_tiles/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="tiles" tilewidth="16" tileheight="16" tilecount="16000" columns="100">
+ <image source="tiles.png" width="1600" height="2560"/>
+</tileset>
